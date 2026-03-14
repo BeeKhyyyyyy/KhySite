2741366ffbe3eb0423f3e404f86ae2747f46ec72
Khyron Rafael <rafaelkhb@students.nu-laguna.edu.ph>
2026-03-14 10:45:51 +0000

    Update app sections, styles, and devcontainer config

---
+++ src/Components/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Education = () => {
+    return (
+        <section id="education" className="min-height">
+            <h2 style={{ textAlign: "center" }}>Education</h2>
+            <div style={{ display: "flex", flexDirection: "column", paddingTop: "3rem" }}>
+                <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+                    <h3>National University Laguna</h3>
+                    <p>Bachelor of Science in Computer Science, 2024-2028</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Education;
